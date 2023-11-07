74f3d14409ad9df7f0d366a1f53e28ffa6b6f63f
Marvin Brereton <marvin.brereton@gmail.com>
2023-11-07 15:02:47 +0000

    Added Register front end

---
+++ Application/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using Core.Models.Account.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Application.Controllers;
+
+public class AccountController : Controller
+{
+    public AccountController()
+    {
+        
+    }
+
+    [HttpGet]
+    [AllowAnonymous]
+    public async Task<IActionResult> Register()
+    {
+        return View();
+    }
+    
+    [HttpPost]
+    [AllowAnonymous]
+    public async Task<IActionResult> Register(RegisterViewModel registerViewModel)
+    {
+        return View();
+    }
+}
