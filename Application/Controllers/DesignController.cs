f8402dc18b5709de05233eb11ca93488c84ca242
Marvin Brereton <marvin.brereton@gmail.com>
2023-11-08 13:44:42 +0000

    Added Anonymous allowance

--- Application/Controllers/DesignController.cs
+++ Application/Controllers/DesignController.cs
@@ -1,19 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Application.Controllers;
 
 public class DesignController : Controller
 {
+    [AllowAnonymous]
     public IActionResult Index()
     {
         return View();
     }    
     
+    [AllowAnonymous]
     public IActionResult Buttons()
     {
         return View();
     }
 
+    [AllowAnonymous]
     public IActionResult Alerts()
     {
         return View();
